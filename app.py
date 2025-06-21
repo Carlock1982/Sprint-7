5113d7dfea675a65c3b46853a95b3bd4b2202d17
Carlock1982 <carlock_6@hotmail.com>
2025-06-21 08:15:51 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Encabezado
+st.header("Panel de Control - Análisis de Vehículos")
+
+# Cargar los datos
+data = pd.read_csv("vehicles_us.csv")
+
+# Botón para histograma
+if st.button("Construir histograma de odómetro"):
+    fig = px.histogram(data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Botón para gráfico de dispersión
+if st.button("Construir gráfico de dispersión"):
+    fig_disp = px.scatter(data, x="odometer", y="price")
+    st.plotly_chart(fig_disp, use_container_width=True)
